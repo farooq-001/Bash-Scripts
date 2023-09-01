f07fc396d9b73dbd1ce90d6a35d947c199a1c7c5
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2023-09-01 16:44:12 +0530

    Update Get-system-resources.sh

--- Get-system-resources.sh
+++ Get-system-resources.sh
@@ -5,6 +5,8 @@ nodes=("node1" "node2" "node3")
 ip_addresses=("XXXXXXXX" "XXXXXXXX" "XXXXXXXX")
 
 # Define your SSH password (consider using SSH key authentication for security)
+# If your getting the error : "./check_disk.sh: line 18: sshpass: command not found"  
+# Please install the package: Ubuntu/Debian: "sudo apt-get install sshpass"  CentOS/RHEL: "sudo yum install sshpass"
 password="XXXXXXXX"
 
 # Function to gather system resource information for a node
