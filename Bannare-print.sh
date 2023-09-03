fb80ea5000dac674da4f598eed742e2125d1288d
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2023-09-03 13:46:07 +0530

    Create Bannare-print.sh

---
+++ Bannare-print.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install figlet and lolcat
+sudo apt install figlet lolcat -y
+
+# Add banner commands to ~/.bashrc
+cat << EOF >> ~/.bashrc
+
+# Print banner
+figlet -f slant -c "lucifer"  | lolcat
+figlet -f digital -c "blusapphire cyberworld"  | lolcat
+
+EOF
+
+# Reload ~/.bashrc to apply changes
+source ~/.bashrc
+
+# Display a message indicating that the script has completed
+echo "Banner setup completed. You can now open a new terminal to see the banners."
