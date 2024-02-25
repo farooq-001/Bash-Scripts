facad1b06be04ba02ccd761c0fd456e1f2075e91
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-02-25 11:09:25 +0530

    Update Bannare-print.sh

--- Bannare-print.sh
+++ Bannare-print.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Install figlet and lolcat
-sudo apt install jp2a -y
-sudo apt install figlet lolcat -y
-
+sudo apt install jp2a -Y
+sudo apt install figlet lolcat -Ymv 
+wget https://t3.ftcdn.net/jpg/05/00/93/20/360_F_500932055_cxYNf3ph03cHWl6r2zRRVMLhF8GqQeeC.jpg
+mv 360_F_500932055_cxYNf3ph03cHWl6r2zRRVMLhF8GqQeeC.jpg logo.png
 # Add banner commands to ~/.bashrc
 cat << EOF >> ~/.bashrc
 
