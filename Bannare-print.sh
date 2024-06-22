1eab8f52ba6a044e01b2d41b0215e4045b4bb43a
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-06-22 13:59:15 +0530

    Update Bannare-print.sh

--- Bannare-print.sh
+++ Bannare-print.sh
@@ -3,6 +3,8 @@
 # Install figlet and lolcat
 sudo apt install jp2a -y
 sudo apt install figlet lolcat -y
+sudo apt install cmatrix -y
+sudo apt install cowsay -y
 
 wget https://t3.ftcdn.net/jpg/05/00/93/20/360_F_500932055_cxYNf3ph03cHWl6r2zRRVMLhF8GqQeeC.jpg
 mv 360_F_500932055_cxYNf3ph03cHWl6r2zRRVMLhF8GqQeeC.jpg /home/.logo.png
@@ -15,6 +17,10 @@ jp2a /home/.logo.png  --color
 export PATH="/usr/games:$PATH"
 figlet -f slant -c "Hi Lucifer"  | lolcat
 figlet -f digital -c "Well come to cyberworld"  | lolcat
+
+# cowsay "Hello 🐮👋"
+# cowsay -f ghostbusters "Hello 👻"
+# cmatrix
 
 EOF
 
